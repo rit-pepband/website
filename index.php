14debaccbfcfc6dfc802a148f05783599d06cfa7
Chris Bentivenga <chris.bentivenga@gmail.com>
2011-11-29 11:40:11 -0500

    Fixed up index.php

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 include '.lib/fammel/fammel.php';
 include '.lib/php_markdown/markdown.php';
 
-$fammel = new Fammel();
-$fammel->parse_file('layout/page.haml');
+render($_GET['page']);
 
-$source= ($fammel->render());
-$content = Markdown(file_get_contents('home.markdown'));
+function render($name){
+    $fammel = new Fammel();
+    $fammel->parse_file('layout/page.haml');
 
-$source = str_replace("[content]",$content,$source);
-echo $source;
+    $source= ($fammel->render());
+    $content = Markdown(file_get_contents('content/' . $name . '.markdown'));
+
+    $source = str_replace("[content]",$content,$source);
+    echo $source;
+}
 ?>
